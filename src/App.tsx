bbae032b032faf2ecc3a3de48c43f7fc2020f041
gfaizank <faizan2017.fk@gmail.com>
2024-03-19 01:38:18 +0530

    code push-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import InputFields from './components/InputFields';
 
-function App() {
+const App: React.FC = () => {
+
+  const [todo, setTodo]=useState<string>("");
+
+  console.log(todo);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className='heading'>Todofy</span>
+      <InputFields todo={todo} setTodo={setTodo} />
     </div>
   );
 }
