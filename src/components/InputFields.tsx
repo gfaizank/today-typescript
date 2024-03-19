6ca9b18bb0576e6cb36a9f4ee637bb8e412a8f64
gfaizank <faizan2017.fk@gmail.com>
2024-03-19 20:40:23 +0530

    Todo UI completed

--- src/components/InputFields.tsx
+++ src/components/InputFields.tsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import "./styles.css"
+import React, { useRef } from "react";
+import "./styles.css";
 
 interface Props {
-    todo: string;
-    setTodo: React.Dispatch<React.SetStateAction<string>>;
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
-const InputFields = ({ todo, setTodo }: Props) => {
+
+const InputFields = ({ todo, setTodo, handleAdd }: Props) => {
+  const inputRef=useRef<HTMLInputElement>(null);
   return (
-    <form className='input'>
-        <input type="input" className="input__box" placeholder="Enter a task" value={todo} onChange={(e)=>{setTodo(e.target.value)}} />
-        <button className='input_submit' type="submit">Go</button>
+    <form className="input" onSubmit={(e)=>{
+      handleAdd(e);
+      inputRef.current?.blur();
+    }}>
+      <input
+       ref={inputRef}
+        type="input"
+        className="input__box"
+        placeholder="Enter a task"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button className="input_submit" type="submit">
+        Go
+      </button>
     </form>
-  )
-}
-export default  InputFields;
+  );
+};
+export default InputFields;
